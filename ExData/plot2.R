b17293abd5be551263a6d928d1744558925b3273
malickdo <malickdo@yahoo.com>
2015-04-12 19:14:08 -0400

    Commit Plot 2

---
+++ ExData/plot2.R
@@ -0,0 +1,37 @@
+# Download the data, unzip, delete zip file
+file.url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+file.dest <- 'power.consumption.zip'
+download.file( file.url, file.dest )
+source.file <- unzip( file.dest, list = TRUE )$Name
+unzip( file.dest )
+file.remove( file.dest )
+rm(file.url)
+rm(file.dest)
+
+# Read the data into R, save, and remove source file
+ElecPowerConsumption <- read.table( source.file, header = TRUE, sep = ';', na.strings = '?' )
+save( ElecPowerConsumption, file = 'ElecPowerConsumption.RData' )
+file.remove( source.file )
+rm(source.file)
+
+# Subset the data
+ElecPowerConsumption$Date <- as.Date(ElecPowerConsumption$Date, format="%d/%m/%Y")
+subset_EPC <- subset(ElecPowerConsumption, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(ElecPowerConsumption)
+
+## Create column - Combo date & time
+datetime <- paste(as.Date(subset_EPC$Date), subset_EPC$Time)
+subset_EPC$date.time <- as.POSIXct(datetime)
+
+# Generate Plot & Save to png file
+GAP <- as.numeric(subset_EPC$Global_active_power)
+png( "plot2.png", height=480, width=480)
+
+plot(subset_EPC$date.time,
+     GAP,
+     type='l',
+     xlab = '',
+     ylab = 'Global Active Power (kilowatts)'
+)
+
+dev.off()
